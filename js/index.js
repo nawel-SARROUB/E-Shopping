b225f0a6fe72df7429c2984313a5126e74d76a8f
jaber <jaberksh3012@gmail.com>
2024-01-23 09:43:49 +0100

    modification index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
-const mostPopPorducts = document.querySelector(".box");
 
+// Select the <section> element to append boxes
+const boxesContainer = document.querySelector("section");
+
+// Path to the JSON file
 const jsonFile = "./products.json";
 
+// Fetch data from the JSON file
 fetch(jsonFile)
-	.then((respone) => {
-		return respone.json();
-	})
-	.then((data) => {
-		data.map((product) => {
-			const { id, name,description, price, images } = product;
-			mostPopPorducts.innerHTML += `
-            <div class="box" data-product="${id}">
+    .then((response) => response.json())
+    .then((data) => {
+        // Iterate through each product in the data
+        data.forEach((product) => {
+            const { id, name, description, price, images } = product;
+
+            // Create a new <div> element for each product
+            const boxElement = document.createElement("div");
+            boxElement.classList.add("box");
+            boxElement.setAttribute("data-product", id);
+
+            // Populate the inner HTML of the box
+            boxElement.innerHTML = `
                 <div class="img">
                     <img src="${images[0].url}" alt="${name}" />
                 </div>
@@ -19,7 +28,10 @@ fetch(jsonFile)
                 </a>
                 <p class="description-articles">${description}</p>
                 <a href="details.html" class="details">Détails</a>
-            </div>
             `;
+
+            // Append the box to the <section>
+            boxesContainer.appendChild(boxElement);
         });
-    });
+    })
+    .catch((error) => console.error("Error fetching data:", error));
