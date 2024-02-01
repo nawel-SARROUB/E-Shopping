6889c2e2d7f37b0bc22d16c44095034cbf4551a9
nawel-SARROUB <nawel.sarroub@gmail.com>
2024-02-01 13:55:08 +0100

    accueil-stylisation des card

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 // Select the <section> element to append boxes
 const boxesContainer = document.querySelector("section");
+boxesContainer.innerHTML = '';
 
 // Path to the JSON file
 const jsonFile = "./products.json";
@@ -13,25 +14,36 @@ fetch(jsonFile)
         data.forEach((product) => {
             const { id, name, description, price, images } = product;
 
-            // Create a new <div> element for each product
-            const boxElement = document.createElement("div");
-            boxElement.classList.add("box");
-            boxElement.setAttribute("data-product", id);
+            // Check if the product data is valid
+            if (id && name && description && images && images[0] && images[0].url) {
+                // Create a new <div> element for each product
+                const boxElement = document.createElement("div");
+                boxElement.classList.add("box");
+                boxElement.setAttribute("data-product", id);
 
-            // Populate the inner HTML of the box
-            boxElement.innerHTML = `
-                <div class="img">
-                    <img src="${images[0].url}" alt="${name}" />
-                </div>
-                <a href="details.html">
-                    <p class="titre-articles">${name}</p>
+                // Populate the inner HTML of the box
+                boxElement.innerHTML = `
+                <a href="details.html?id=${id}" class="details">
+                    <img src="${images[0].url}" alt="${name}" class="images-style" />
                 </a>
-                <p class="description-articles">${description}</p>
-                <a href="details.html?id=${id}" class="details">Détails</a>
-            `;
+                <div class="part-infos">
+                    <a href="details.html">
+                        <p class="titre-articles">${name} - ${description}</p>
+                    </a>
+                    <div class="ajout-button">
+                        <p class="price-articles">${price}</p>
+                        <i class="material-symbols-outlined icon-card">favorite</i>
+                    </div>
+                </div>
+                `;
 
-            // Append the box to the <section>
-            boxesContainer.appendChild(boxElement);
+                // Append the box to the <section>
+                boxesContainer.appendChild(boxElement);
+            }
+
+            
         });
     })
     .catch((error) => console.error("Error fetching data:", error));
+
+    // !!! penser à changer l'url et mettre le nom de l'article
